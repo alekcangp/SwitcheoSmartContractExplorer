989d3de7f4e6e31f5de3c9b43b5a2206e2837f65
alekcangp <40195312+alekcangp@users.noreply.github.com>
2018-10-06 18:35:54 +0600

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -10,12 +10,15 @@ address: "",
 results: [],
 contract: "91b83e96f2a7c4fdf0c1688441ec61986c7cae26"
 },
-//Request API
+//Request data API
 methods: {
-    request: function (event) {
-	var addr = decode(vm.address);
-	var url ="https://api.switcheo.network/v2/balances?addresses="+addr+"&contract_hashes="+vm.contract;
-	axios.get(url).then(response => {this.results = response.data});    
+    request: 
+		function (event) {
+			this.results = [];
+			document.getElementById('txt').innerHTML = "";
+			var addr = decode(vm.address);
+			var url ="https://api.switcheo.network/v2/balances?addresses="+addr+"&contract_hashes="+vm.contract;
+			axios.get(url).then(response => {this.results = response.data});   
     }
 },
 // Save Neo address in local storage
@@ -31,3 +34,4 @@ mounted() {
   }
   
 });
+
