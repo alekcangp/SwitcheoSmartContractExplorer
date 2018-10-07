7898610396cc9f619d7a6c673f49c7d0ed4b2306
alekcangp <40195312+alekcangp@users.noreply.github.com>
2018-10-07 18:36:47 +0600

    Add get new tokens by API

--- src/api.js
+++ src/api.js
@@ -8,30 +8,40 @@ el: '#api',
 data: {
 address: "",
 results: [],
-contract: "91b83e96f2a7c4fdf0c1688441ec61986c7cae26"
+tokens: [],
+contract: "&contract_hashes[]=91b83e96f2a7c4fdf0c1688441ec61986c7cae26&contract_hashes[]=01bafeeafe62e651efc3a530fde170cf2f7b09bd&contract_hashes[]=0ec5712e0f7c63e4b0fea31029a28cea5e9d551f",
 },
+
 //Request data API
 methods: {
     request: 
 		function (event) {
-			this.results = [];
-			document.getElementById('txt').innerHTML = "";
+			document.getElementById('txt').innerHTML = ""; //delete alert message
+			var data;
 			var addr = decode(vm.address);
-			var url ="https://api.switcheo.network/v2/balances?addresses="+addr+"&contract_hashes="+vm.contract;
-			axios.get(url).then(response => {this.results = response.data});   
-    }
+			var url = "https://api.switcheo.network/v2/balances?addresses="+addr+vm.contract;
+			var urll = "https://api.switcheo.network/v2/exchange/tokens"
+			vm.results = []; // to update table			
+			axios.get(urll).then(function(response) {	   //get json tokens
+			vm.tokens = response.data;
+				}
+			); 
+			axios.get(url).then(function(response) {       // get json balances
+			vm.results = response.data;
+				}
+			); 
+		}
 },
 // Save Neo address in local storage
 mounted() {
-    if (localStorage.address) {
+	  if (localStorage.address) {
       this.address = localStorage.address;
     }
-  },
-  watch: {
+},
+watch: {
     address(newAddress) {
-      localStorage.address = newAddress;
+    localStorage.address = newAddress;
     }
-  }
-  
-});
+}
+})
 
